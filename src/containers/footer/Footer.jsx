707655370c591989b6b10209499d9f8df5ba0b2f
Oussa-Err <dexterlecter0@gmail.com>
2023-09-20 17:05:25 +0100

    update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -43,7 +43,7 @@ function Footer () {
         </div>
       </div>
       <div className='gpt3__footer-copyright'>
-        <p>© 2023&nbsp;<span className='footer_err'><a href="https://github.com/oussa-err" target='_blank'>Err.</a></span></p>
+        <div>© 2023&nbsp;<span className='footer_err'><a  href="https://github.com/oussa-err" target='_blank'>Err.</a></span></div>
       </div>
     </div>
   )
